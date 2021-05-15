099990db4b7274133f935ab13731143fcc243c92
Joshua Draxten <josh@pop-os.localdomain>
2021-05-15 15:05:39 -0500

    Add basic styles

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,12 +10,18 @@ function SearchBar({ handleSearch }: { handleSearch: Function }){
     }
 
     return (
-        <div>
+        <div className="flex my-8">
             <input
+                className="flex-grow border-2 border-grey rounded-md p-1 mr-2"
                 value={searchRequest}
                 onChange={e => setSearchRequest(e.target.value)}
                 onKeyDown={searchOnReturnKeyPress}/>
-            <button onClick={()=>handleSearch(searchRequest)}>Search!</button>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={()=>handleSearch(searchRequest)}
+            >
+                Search!
+            </button>
         </div>
     )
 }
