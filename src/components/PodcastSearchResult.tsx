b849b61c0e6fb864c0cbb855b202e2d6cc89278c
Joshua Draxten <joshuadraxten@live.com>
2021-05-15 16:00:49 -0500

    Add blank alt to podcast artwork

--- src/components/PodcastSearchResult.tsx
+++ src/components/PodcastSearchResult.tsx
@@ -3,7 +3,7 @@ import { itunesTrack } from '../types';
 
 export default function podcastResult(props: { result: itunesTrack }){
     return <div className="flex items-center py-2">
-        <img src={props.result.artworkUrl100} style={{width: 100, height: 100}} />
+        <img src={props.result.artworkUrl100} alt="" style={{width: 100, height: 100}} />
         <div className="ml-4">
             <h3 className="font-bold text-lg">{props.result.collectionName}</h3>
             <p>{props.result.artistName}</p>
